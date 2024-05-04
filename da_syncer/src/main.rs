a9babbfff366423c4ee6605ec7c3695fbe2aa4c5
Adam Perkowski <adas1per@protonmail.com>
2024-05-04 15:09:10 +0200

    startup

--- da_syncer/src/main.rs
+++ da_syncer/src/main.rs
@@ -1,3 +1,25 @@
+// Copyleft 🄯 2024  PW-Sync Team
+
+use std::{ fs::metadata, env, process };
+use is_root::is_root;
+
+pub fn path_exists(path: &str) -> bool {
+    metadata(path).is_ok()
+}
+
 fn main() {
-    println!("Hello, world!");
+    if is_root() {
+        println!(" < !! > nuh uh. i no want be root!");
+        process::exit(1);
+    }
+
+    let mut conf = env::var("XDG_CONFIG_HOME").unwrap();
+
+    println!(" < Para Wodna Sync for Linux >\n");
+
+    conf = format!("{conf}/pwsync");
+    if !path_exists(&conf) {
+        println!(" < !! > PWSync config not found. Did you use the install script?");
+        process::exit(2);
+    }
 }
